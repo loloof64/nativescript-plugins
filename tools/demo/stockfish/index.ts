ee422f75f1b4920e2e53db539bad55defa4001b3
loloof64 <laurent.bernabe@gmail.com>
2022-08-01 21:19:55 +0200

    Set up stockfish demo UI

--- tools/demo/stockfish/index.ts
+++ tools/demo/stockfish/index.ts
@@ -2,7 +2,11 @@ import { DemoSharedBase } from '../utils';
 import {} from '@loloof64/stockfish';
 
 export class DemoSharedStockfish extends DemoSharedBase {
-  testIt() {
+  evaluate() {
     console.log('test stockfish!');
+  }
+
+  onSliderValueChanged(event) {
+    console.log(`Slider changed : ${event}`);
   }
 }
