ac5be1211c4242259feba29cfa932ed8c7bee683
loloof64 <laurent.bernabe@gmail.com>
2022-08-02 10:35:54 +0200

    Adding a bit of interactivity to the stockfish demo app

--- apps/demo/src/plugin-demos/stockfish.ts
+++ apps/demo/src/plugin-demos/stockfish.ts
@@ -7,4 +7,30 @@ export function navigatingTo(args: EventData) {
   page.bindingContext = new DemoModel();
 }
 
-export class DemoModel extends DemoSharedStockfish {}
+export class DemoModel extends DemoSharedStockfish {
+  positionFen: string;
+  thinkingTimeMs: number;
+
+  constructor() {
+    super();
+    this.positionFen = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1';
+    this.thinkingTimeMs = 500;
+  }
+
+  evaluate() {
+    console.log(`Evaluation for position : ${this.positionFen}`);
+    console.log(`Thinking time ms : ${this.thinkingTimeMs}`);
+  }
+
+  pasteFen(args: EventData) {
+    console.log(args);
+  }
+
+  onSliderValueChanged(args: EventData) {
+    console.log(args);
+  }
+
+  onPositionChanged(args: EventData) {
+    console.log(args);
+  }
+}
--- tools/demo/stockfish/index.ts
+++ tools/demo/stockfish/index.ts
@@ -1,12 +1,4 @@
 import { DemoSharedBase } from '../utils';
 import {} from '@loloof64/stockfish';
 
-export class DemoSharedStockfish extends DemoSharedBase {
-  evaluate() {
-    console.log('test stockfish!');
-  }
-
-  onSliderValueChanged(event) {
-    console.log(`Slider changed : ${event}`);
-  }
-}
+export class DemoSharedStockfish extends DemoSharedBase {}
